898c9b387de5665f1747f06b5c0bbc67b0c8c115
Kunal-023 <kunal031016@gmail.com>
2024-07-27 01:04:24 +0530

    first commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./Header.css"
+import { Link } from "react-router-dom"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="headerLeft">
+                <Link to="/"><img className="header__icon" src="https://banner2.cleanpng.com/20190630/ogy/kisspng-film-computer-icons-vector-graphics-clip-art-logo-fotografie-thun-chic-shot-productions-travel-5d1834d4a755c7.4505562315618674766854.jpg" /></Link>
+                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
+                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header
